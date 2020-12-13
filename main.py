382c9c593fe6b092832abf6066a89fcf6b962092
Dhadhazi <dhadhazi@gmail.com>
2020-12-13 11:31:35 +0100

    Movie list project done

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://news.ycombinator.com/")
+response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
 
-yc_web_page = response.text
+soup = BeautifulSoup(response.text, "html.parser")
 
-soup = BeautifulSoup(yc_web_page, "html.parser")
+titles = soup.find_all(name="h3", class_="title")
 
-articles = soup.find_all(name="a", class_="storylink")
-article_texts = []
-article_links = []
-for article_tag in articles:
-    article_texts.append(article_tag.getText())
-    article_links.append(article_tag.get("href"))
-article_upvote = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+movie_string = "1) "
+for i in range(len(titles)-1, -1, -1):
+    movie_string += (titles[i].getText())+"\n"
 
-print(article_texts)
-print(article_links)
-print(article_upvote)
-
-max_points = max(article_upvote)
-index_of_max_points = article_upvote.index(max_points)
-print(article_texts[index_of_max_points])
-print(article_links[index_of_max_points])
+with open("movies.txt",mode="w") as file:
+    file.write(movie_string)
